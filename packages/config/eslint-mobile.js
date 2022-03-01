7e7226e0d80d7adfc699130e110845efa5e0204d
Warren Layson <warrenlayson@gmail.com>
2022-03-01 15:57:37 +0800

    Recreated mobile app
    Regenerated mobile app with expo template tabs, eslint

--- packages/config/eslint-mobile.js
+++ packages/config/eslint-mobile.js
@@ -9,15 +9,6 @@ module.export = {
     'plugin:prettier/recommended',
   ],
   plugins: ['@typescript-eslint', 'react', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
   rules: {
     'import/no-unresolved': 0,
     'react/jsx-filename-extension': [
